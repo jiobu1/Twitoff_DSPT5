3c56d57c756af21e200e2eeb200c8e4d9f45885a
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-07 23:41:47 -0400

    Update twitter_service

--- web_app/services/twitter_service.py
+++ web_app/services/twitter_service.py
@@ -1,9 +1,9 @@
 #web_app/services/twitter_service.py
 
 import os
-import tweepy
-
+from pprint import pprint
 from dotenv import load_dotenv
+import tweepy
 
 load_dotenv()
 
@@ -12,7 +12,6 @@ TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
 TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
-
 auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
 auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
 print("AUTH", type(auth))
@@ -20,15 +19,21 @@ print("AUTH", type(auth))
 api = tweepy.API(auth)
 print("API CLIENT", type(api))
 
-user = api.get_user("s2t2")
-print(type(user)) #> <class 'tweepy.models.User'>
+#def twitter_api_client():
+#    return tweepy.API(auth)
 
-from pprint import pprint
-pprint(user._json)
-print(user.id)
-print(user.screen_name)
-print(user.name)
-print(user.followers_count)
+if __name__ == "__main__":
+    user = api.get_user("s2t2")
+    print(type(user)) #> <class 'tweepy.models.User'>
+    pprint(user._json)
+    print(user.id)
+    print(user.screen_name)
+    print(user.name)
+    print(user.followers_count)
+    #tweets = api.user_timeline("s2t2", tweet_mode="extended", count=150, exclude_replies=True, include_rts=False)
+    tweets = api.user_timeline("s2t2", tweet_mode="extended", count=150)
+    print(type(tweets)) #> <class 'tweepy.models.ResultSet'>
+    for tweet in tweets:
+        print("-----")
+        print(tweet.id, tweet.full_text)
 
-
-breakpoint()
