f7dfdb0664a2e93a878e6119a234e9de84f0ec8c
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-07 22:50:48 -0400

    Basilica

--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -7,11 +7,11 @@ load_dotenv()#parse the .env file for environment variable
 
 BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
 
-connection =  basilica.Connection("BASILICA_API_KEY")
+connection =  basilica.Connection(BASILICA_API_KEY)
 print(type(connection))#> <basilica.Connection object at 0x102081b10>
 
 if __name__ == "__main__":
-    
+
     sentences = ["Hello world!", "How are you?"]
     embeddings = connection.embed_sentences(sentences)
     print(list(embeddings)) # [[0.8556405305862427, ...], ...]
