aeb4bd3f0ba1d97e7743d2bbd2a7f07fb4287d55
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-09 19:56:16 -0400

    Update

---
+++ web_app/routes/admin_routes.py
@@ -0,0 +1,24 @@
+# web_app/routes/admin_routes.py
+
+from flask import Blueprint, jsonify, request , render_template, flash, redirect
+
+from flask_app.models import db
+
+admin_routes = Blueprint("admin_routes", __name__)
+
+API_KEY = "abc123" # TODO: set as secret env var
+
+
+# GET /admin/db/reset?api_key=abc123
+@admin_routes.route("/admin/db/reset")
+def reset_db():
+    print("URL PARMS", dict(request.args))
+
+    if "api_key" in dict(request.args) and request.args["api_key"] == API_KEY:
+        print(type(db))
+        db.drop_all()
+        db.create_all()
+        return jsonify({"message": "DB RESET OK"})
+    else:
+        flash("OOPS Permission Denied", "danger")
+        return redirect("/books")
