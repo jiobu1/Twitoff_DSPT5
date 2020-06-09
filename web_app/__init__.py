d336ebbfef3c64a12abd421d51b1bb3411f0e593
Jisha Obukwelu <jisha@obukwelu.com>
2020-06-08 22:53:05 -0400

    Example classifier

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,6 +5,8 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
+
 
 DATABASE_URI = "sqlite:///twitoff_development_pt5.db" # using relative filepath
 
@@ -18,6 +20,8 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(twitter_routes)
+
     
     return app
 
